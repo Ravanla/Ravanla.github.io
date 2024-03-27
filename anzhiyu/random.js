02d5b6e28dc56853b8337156f81d76f5d41a1ced
Ravanla <1285644869@qq.com>
2024-03-27 22:33:16 +0800

    Site updated: 2024-03-27 22:33:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/26/hello-world/"];function toRandomPost(){
+var posts=["2024/03/27/My-New-Post/","2024/03/26/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
