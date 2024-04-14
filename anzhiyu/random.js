c4744f420144e67139ee8f72ba46695a1d97b79e
Ravanla <1285644869@qq.com>
2024-04-15 07:56:45 +0800

    Site updated: 2024-04-15 07:56:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/27/2024-03-27-My-First-Post/","2024/03/26/2024-03-26-hello-world/","2024/03/28/2024-03-28-传盛工作/"];function toRandomPost(){
+var posts=["2024/03/27/2024-03-27-My-First-Post/","2024/03/28/2024-03-28-传盛工作/","2024/04/15/2024-04-15-宏模版/","2024/03/26/2024-03-26-hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
